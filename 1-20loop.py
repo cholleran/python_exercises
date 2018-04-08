b85d6db010204a401bc1649919dc7137b08d796e
cholleran <36338454+cholleran@users.noreply.github.com>
2018-04-08 22:06:10 +0100

    Update 1-20loop.py

--- 1-20loop.py
+++ 1-20loop.py
@@ -1,9 +1,12 @@
+
 #Adapted from:https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 # This program uses a 'for' loop to find the lowest number evenly divisible by all numbers from 1-20.
 # Student: Cormac Holleran //GMIT - Module: 52167
 
 one_to_twenty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18, 19, 20]
 
+#Checks numbers from 20 to 10**10(in increments of 20) to find the lowest number that 'all' numbers in the list are evenly divisible by.
+#When the program finds the lowest number evenly divisible, the 'break' command exits the loop.
 for num in range(20, 10**10, 20):
   if all(num % n == 0 for n in one_to_twenty):
     break 
