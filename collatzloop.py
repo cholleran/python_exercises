d62d9a36c6cc954c596e69217443e3550902b704
cholleran <36338454+cholleran@users.noreply.github.com>
2018-04-08 21:42:44 +0100

    Update collatzloop.py

--- collatzloop.py
+++ collatzloop.py
@@ -3,8 +3,11 @@
 # Week 3 exercise - program which applies Collatz to an integer chosen by the user.
 # Student: Cormac Holleran, GMIT - Module: 52167
 
+#input is taken from the user and stored as n.
 n = int(input('Please enter and integer:'))
 
+# Uses Boolean statements to apply the rules of Collatz conjecture.
+# The 'while' keyword ensures the program runs until the number reaches 1.
 while n != 1:
     if n % 2 == 0:
         n = n / 2
