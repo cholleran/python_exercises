cee0bb370aec7857605d8e9da121f0971a670f28
cholleran <36338454+cholleran@users.noreply.github.com>
2018-04-08 21:35:16 +0100

    Update openpython.py

--- openpython.py
+++ openpython.py
@@ -1,6 +1,12 @@
 
 # GMIT Module 52167 - Exercise 5 - Student: Cormac Holleran
 #https://stackoverflow.com/questions/275018/how-can-i-remove-chomp-a-trailing-newline-in-python
+#'with open' opens the iris file and closes it again after use.
+#A for loop is used to iterate through the file.
+#Column variables are created using index slicing.
+# The columns are put together in a table variable and formatted.
+#The rstrip command is used to remove the extra line character.
+
 with open ("Data\iris.csv") as f:
     for line in f:
       col_1 = (line.split(',')[0])
